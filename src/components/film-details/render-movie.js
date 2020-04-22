84f6a0f47c1ca873976c30da7bec323471b8803f
Julie Gankina <qunya11@gmail.com>
2020-04-23 01:18:13 +0300

    add esc event listeners for details block

--- src/components/film-details/render-movie.js
+++ src/components/film-details/render-movie.js
@@ -1,9 +1,10 @@
 import FilmDetailsComponent from "./film-details.js";
 import {Position} from "../consts.js";
-import {render} from "../utils.js";
+import {render, escKeyHandler} from "../utils.js";
 
 const closeBtnHandler = (block) => {
   document.body.removeChild(block.getElement());
+
 };
 
 export const renderMovie = (container, movie) => {
@@ -11,7 +12,15 @@ export const renderMovie = (container, movie) => {
   render(container, filmDetailsBlock.getElement(), Position.AFTEREND);
 
   const closeBtn = filmDetailsBlock.getElement().querySelector(`.film-details__close-btn`);
+
   closeBtn.addEventListener(`click`, () => {
     closeBtnHandler(filmDetailsBlock);
+    document.removeEventListener(`keydown`, escKeyHandler);
+  });
+
+  document.addEventListener(`keydown`, (evt) => {
+    escKeyHandler(evt, () => {
+      closeBtnHandler(filmDetailsBlock)
+    });
   });
 };
